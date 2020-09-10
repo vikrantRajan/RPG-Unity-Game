8e167b26fa5e612fa69aa65e0df0cd44af1790d8
Vikrant Rajan <vikrant_rajan@thecdm.ca>
2020-09-09 21:26:56 -0700

    Player movement system

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToClickedPoint();
+        }
+    }
+
+    private void MoveToClickedPoint() 
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit) 
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+
+    }
+}
