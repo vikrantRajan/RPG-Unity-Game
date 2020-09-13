6aef0d580f527e4842e7722f79b353289ad9876a
Vikrant Rajan <vikrant_rajan@thecdm.ca>
2020-09-12 20:34:50 -0700

    Added Combat Logic

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using RPG.Movement;
+using RPG.Combat;
 using UnityEngine;
 
 namespace RPG.Control 
@@ -6,6 +7,27 @@ namespace RPG.Control
     public class PlayerController : MonoBehaviour
     {
         private void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+           RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+           foreach (RaycastHit hit in hits)
+           {
+              CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+              if (target == null) continue;
+
+              if(Input.GetMouseButtonDown(0))
+              {
+                  GetComponent<Fighter>().Attack(target);
+              }
+           }
+        }
+
+        private void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
@@ -15,15 +37,19 @@ namespace RPG.Control
 
         private void MoveToClickedPoint()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
 
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
 
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
